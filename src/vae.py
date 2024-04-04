8ad0d8777c18e1fb6da14f369f2d9c9321ac9e73
Yann Meunier <yann.meunier@insa-lyon.fr>
2024-04-04 14:27:03 +0200

    Ajout de vae.py pour utiliser plus simplement la classe vae

---
+++ src/vae.py
@@ -0,0 +1,75 @@
+       
+import os
+from PIL import Image
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms 
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data import Subset, SubsetRandomSampler
+import torch.nn.functional as F
+
+# VAE model
+class VAE(nn.Module):
+    def __init__(self, input_channels, latent_dim):
+        super(VAE, self).__init__()
+        
+        self.encoder = nn.Sequential(
+            nn.Conv2d(input_channels, 32, 4, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 4, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, 4, 2, 1),
+            
+            nn.ReLU(),
+            nn.Conv2d(128, 256, 4, 2, 1),
+            nn.ReLU()
+        )
+        
+        self.fc_mu = nn.Linear(256 * 4 * 4, latent_dim)
+        self.fc_logvar = nn.Linear(256 * 4 * 4, latent_dim)
+        
+        self.decoder_fc = nn.Linear(latent_dim, 256 * 4 * 4)
+        
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, 4, 2, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, 4, 2, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, 4, 2, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, input_channels, 4, 2, 1),
+            nn.Sigmoid()
+        )
+        
+    def encode(self, x):
+        x = self.encoder(x)
+        x = x.view(x.size(0), -1)
+        mu = self.fc_mu(x)
+        logvar = self.fc_logvar(x)
+        return mu, logvar
+    
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+    
+    def decode(self, z):
+        z = self.decoder_fc(z)
+        z = z.view(z.size(0), 256, 4, 4)
+        return self.decoder(z)
+    
+    def forward(self, x, attr):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        recon_x = self.decode(z)
+        return recon_x, mu, logvar
+
+
+#Loss function
+def vae_loss(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x.view(-1, input_channels*64*64), x.view(-1, input_channels*64*64), reduction='sum')
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return BCE + KLD
