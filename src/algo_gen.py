2b62d5160f8ee1b722190a5c18e65326eeb446ac
tspitzinsalyon <thimote.spitz@insa-lyon.fr>
2024-04-10 22:11:25 +0200

    ajout des params de l'algo gen

--- src/algo_gen.py
+++ src/algo_gen.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import torch
 import vae
 import os
+import numpy
 
 model = vae.load_model()
 
+# Paramètres de l'algorithme génétique
+taille_vecteur = 128
+taux_mutation = 0.3
+nombre_nouveaux_individus = 20  # Générer 20 vecteurs enfants 
