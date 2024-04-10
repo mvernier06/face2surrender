660ffd16364b6086c6dd734ef1a9ac4bfa1586c9
tspitzinsalyon <thimote.spitz@insa-lyon.fr>
2024-04-10 22:28:03 +0200

    Ajout fonction de croisement

--- src/algo_gen.py
+++ src/algo_gen.py
@@ -3,11 +3,34 @@ import matplotlib.pyplot as plt
 import torch
 import vae
 import os
-import numpy
+import numpy as np
+from vae import latent_dim
 
 model = vae.load_model()
 
 # Paramètres de l'algorithme génétique
-taille_vecteur = 128
+taille_vecteur = 500 # dimension des vecteurs latents définis dans vae
 taux_mutation = 0.3
+taux_croisement = 0.2
 nombre_nouveaux_individus = 20  # Générer 20 vecteurs enfants 
+
+# Fonction de croisement intercalé entre les vecteurs latents
+def croisement_intercalé(parents):
+    taille_segment = int(taille_vecteur * taux_croisement)
+    if taille_segment < 1:
+        return print("Les vecteurs latents n'ont pas pu être lus ou sont de dimension inférieure au seuil taux de croisement * dimension.")
+    nouveaux_individus = []
+    for i in range(nombre_nouveaux_individus):
+        nouvel_individu = np.zeros(taille_vecteur)
+        position_actuelle = 0
+        for j, parent in enumerate(parents):
+            fin_segment = position_actuelle + taille_segment
+            if fin_segment > taille_vecteur:
+                fin_segment = taille_vecteur
+            segment = parent[position_actuelle:fin_segment]
+            nouvel_individu[position_actuelle:fin_segment] = segment
+            position_actuelle += taille_segment
+            if position_actuelle >= taille_vecteur:
+                break
+        nouveaux_individus.append(nouvel_individu)
+    return nouveaux_individus
